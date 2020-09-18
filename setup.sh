c2429fab0d02dd9147534f99363098384635e861
kopidadoudou <kelvintee1999@gmail.com>
2020-09-19 05:47:19 +0800

    hello

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"1161104595@student.mmu.edu.com\"\n\
-" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+\n\
 " > ~/.streamlit/config.toml
